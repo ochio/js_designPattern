95f86ca327151f60fe28d5b70cf22f86636a3dbc
ochi <ocos54ce@gmail.com>
2021-05-08 22:30:39 +0900

    コマンドパターン

---
+++ command.js
@@ -0,0 +1,18 @@
+const CarManager = {
+	requestInfo: function(model, id) {
+		return `The information for ${model} with ID ${id} is foobar`
+	},
+	buyVehucle: function(model, id) {
+		return `You have successfully purchased Item ${id}, a ${model}`
+	},
+	arrangeViewing: function(model, id) {
+		console.log(this);
+		return `You have successfully booked a viewing of ${model} ${id}`
+	}
+}
+
+CarManager.execute = function (name){
+	return CarManager[name] && CarManager[name].apply({a:1}, [].slice.call(arguments, 1))
+}
+
+console.log(CarManager.execute('arrangeViewing', 'フェラーリ', "12345"));
